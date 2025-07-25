60c55551be9f91f477cc6ebdeb5af7d63f2a03d4
Sanjay <sanjay3005v@gmail.com>
2025-07-25 15:56:59 +0000

    the arrows are overlapping in the ui now

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -25,7 +25,7 @@ function Calendar({
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
-        caption: "flex justify-center pt-1 relative items-center",
+        caption: "flex justify-between pt-1 relative items-center",
         caption_label: "text-sm font-medium",
         caption_dropdowns: "flex justify-center gap-1",
         nav: "space-x-1 flex items-center",
@@ -33,8 +33,8 @@ function Calendar({
           buttonVariants({ variant: "outline" }),
           "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
         ),
-        nav_button_previous: "absolute left-1",
-        nav_button_next: "absolute right-1",
+        nav_button_previous: "h-7 w-7",
+        nav_button_next: "h-7 w-7",
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
         head_cell:
@@ -127,3 +127,4 @@ import { format, setMonth } from "date-fns";
 
 
 export { Calendar }
+
