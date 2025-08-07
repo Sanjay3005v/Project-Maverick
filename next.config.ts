77b6bdbaef07a9dd42204ac34f930537279cef2d
Sanjay <sanjay3005v@gmail.com>
2025-08-07 13:50:31 +0000

    login-form.tsx:141  POST https://identitytoolkit.googleapis.com/v1/acco

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -10,6 +11,10 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
